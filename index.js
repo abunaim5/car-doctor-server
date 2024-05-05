84140c5bde55d17fd6d5adb6c07d175027559f86
abunaim5 <abunaim140@gmail.com>
2024-05-05 20:47:32 +0600

    implement add bookings and read bookings by specific service id

--- index.js
+++ index.js
@@ -46,11 +46,27 @@ async function run() {
             res.send(result);
         });
 
-        // booking services
-        app.post('/bookings', async(req, res) => {
+        // booking
+        app.get('/bookings', async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
+        })
+
+        app.delete('/bookings/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
